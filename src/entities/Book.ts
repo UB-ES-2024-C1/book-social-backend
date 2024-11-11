88659f15c52be5f45087943d0d44401eefe45ce2
Josep Arnau Belmonte Juliachs <abelmoju11@alumnes.ub.edu>
2024-11-11 22:17:32 +0100

    Afegir alguns missatges d'error a la entitat

--- src/entities/Book.ts
+++ src/entities/Book.ts
@@ -24,6 +24,14 @@ export class Book {
   })
   author!: string;
 
+  @Column({ type: 'date' })
+  @IsDate({ message: 'Publication date must be a valid date' })
+  publication_date!: Date;
+
+  @Column({ length: 100 })
+  @Length(1, 100, { message: 'Genre must be up to 100 characters' })
+  genre!: string;
+
   @Column('text', { nullable: true })
   synopsis?: string;
 
@@ -31,18 +39,12 @@ export class Book {
   @IsUrl({}, { message: 'Image URL must be a valid URL' })
   image_url?: string;
 
-  @Column({ type: 'date' })
-  @IsDate({ message: 'Publication date must be a valid date' })
-  publication_date!: Date;
-
-  @Column({ length: 100 })
-  genre!: string;
-
   @Column({ nullable: true })
   @IsInt({ message: 'Number of pages must be an integer' })
   @Min(1, { message: 'Number of pages must be at least 1' })
   num_pages?: number;
 
   @Column({ length: 255, nullable: true })
+  @Length(0, 255, { message: 'Publisher must be up to 255 characters' })
   publisher?: string;
 }
