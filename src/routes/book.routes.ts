b90463916f14d97d469af0366665451bd3628750
APenalba <apenalbatarrida@gmail.com>
2024-11-30 14:55:18 +0100

    book routes extended

--- src/routes/book.routes.ts
+++ src/routes/book.routes.ts
@@ -3,11 +3,47 @@ import {
   getBookInfo,
   getListOfBooks,
   create,
+  updateBook,
+  deleteBook,
+  searchBooksHandler,
+  getBooksByGenreHandler,
+  getBooksByAuthorHandler,
+  getBookReviewsHandler,
+  getBookRatingStatsHandler,
+  getTopRatedBooksHandler,
+  getAllCategoriesHandler,
+  getAllGenresHandler,
+  getRecentBooksHandler,
+  getBookStatsHandler,
 } from '../controllers/book.controller';
+
 const router = express.Router();
 
-router.get('/book-detail/:id', getBookInfo);
-router.post('/book-list/', getListOfBooks);
+// Existing routes
+router.get('/book-detail/:bookId', getBookInfo);
+router.get('/', getListOfBooks);
 router.post('/', create);
+
+// New CRUD routes
+router.put('/:bookId', updateBook);
+router.delete('/:bookId', deleteBook);
+
+// New search and filter routes
+router.get('/search', searchBooksHandler);
+router.get('/genre/:genre', getBooksByGenreHandler);
+router.get('/author/:authorId', getBooksByAuthorHandler);
+
+// Reviews and Ratings routes
+router.get('/:bookId/reviews', getBookReviewsHandler);
+router.get('/:bookId/rating-stats', getBookRatingStatsHandler);
+router.get('/top-rated', getTopRatedBooksHandler);
+
+// Categorization routes
+router.get('/categories', getAllCategoriesHandler);
+router.get('/genres', getAllGenresHandler);
+router.get('/recent', getRecentBooksHandler);
+
+// Statistics routes
+router.get('/stats', getBookStatsHandler);
 
 export default router;
